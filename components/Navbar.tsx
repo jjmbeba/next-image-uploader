7128d4086bcf06d29382cd5e6d8d892c63d93440
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2024-08-31 15:57:35 +0300

    Add theme provider - dark theme

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,14 @@
 import {Separator} from "@/components/ui/separator";
 import Image from 'next/image';
+import logo from '@/public/logo.svg';
+import {ThemeToggle} from "@/components/ThemeToggle";
 
 const Navbar = () => {
     return (
         <>
             <div className={'flex items-center justify-between px-12 py-5'}>
-                <Image src={'/logo.svg'} alt={'Logo'}/>
-                {/*<ThemeToggle/>*/}
+                <Image src={logo} width={140} height={200} alt={'Logo'}/>
+                <ThemeToggle/>
             </div>
             <Separator className={'w-full h-[1px]'} />
         </>
